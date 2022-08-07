c280d8b0e46afccea667dd47add376840ee64d42
mgrami <m.grami.m@gmail.com>
2022-08-07 11:22:43 -0700

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const path = require("path")
+const cors = require("cors")
+const http = require("http")
+const { Server } = require("socket.io")
+const express = require("express")
+
+///////////////////////////////////////////////
+const app = express()
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+})
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+})
+///////////////////////////////////////////////
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 3001, () => {
+  console.log("SERVER RUNNING");
+});
